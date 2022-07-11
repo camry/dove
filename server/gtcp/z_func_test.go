b813128c06eab701f86525fba4ce7b5fe30cea19
Camry.Chen <camry.chen@foxmail.com>
2022-07-11 19:25:32 +0800

    新增 TCP 服务器测试用例

---
+++ server/gtcp/z_func_test.go
@@ -0,0 +1,24 @@
+package gtcp_test
+
+import (
+    "github.com/camry/dove/server/gtcp"
+    "reflect"
+    "testing"
+)
+
+func TestGetFreePort(t *testing.T) {
+    _, err := gtcp.GetFreePort()
+    if err != nil {
+        t.Error(err)
+    }
+}
+
+func TestGetFreePorts(t *testing.T) {
+    ports, err := gtcp.GetFreePorts(2)
+    if err != nil {
+        t.Error(err)
+    }
+    if !reflect.DeepEqual(len(ports), 2) {
+        t.Fatalf("len(ports):%d is not equal to v:2", len(ports))
+    }
+}
---
+++ server/gtcp/z_server_test.go
@@ -0,0 +1,69 @@
+package gtcp_test
+
+import (
+    "context"
+    "fmt"
+    "github.com/camry/dove/server/gtcp"
+    "reflect"
+    "testing"
+    "time"
+)
+
+var (
+    simpleTimeout = time.Millisecond * 100
+    sendData      = []byte("hello")
+)
+
+func TestNewServer(t *testing.T) {
+    var (
+        port, _ = gtcp.GetFreePort()
+        ctx     = context.Background()
+        addr    = fmt.Sprintf("127.0.0.1:%d", port)
+    )
+    s := gtcp.NewServer(gtcp.Address(addr), gtcp.Handler(func(conn *gtcp.Conn) {
+        defer conn.Close()
+        for {
+            data, err := conn.Receive(-1)
+            if err != nil {
+                break
+            }
+            conn.Send(data)
+        }
+    }))
+    go s.Start(ctx)
+    time.Sleep(simpleTimeout)
+    receive, err := gtcp.SendReceive(addr, sendData, -1)
+    if err != nil {
+        t.Error(err)
+    }
+    if !reflect.DeepEqual(receive, sendData) {
+        t.Fatalf("%s is not equal to v:%s", string(receive), string(sendData))
+    }
+}
+
+func TestNewPkgServer(t *testing.T) {
+    var (
+        port, _ = gtcp.GetFreePort()
+        ctx     = context.Background()
+        addr    = fmt.Sprintf("127.0.0.1:%d", port)
+    )
+    s := gtcp.NewServer(gtcp.Address(addr), gtcp.Handler(func(conn *gtcp.Conn) {
+        defer conn.Close()
+        for {
+            data, err := conn.ReceivePkg()
+            if err != nil {
+                break
+            }
+            conn.SendPkg(data)
+        }
+    }))
+    go s.Start(ctx)
+    time.Sleep(simpleTimeout)
+    receive, err := gtcp.SendReceivePkg(addr, sendData)
+    if err != nil {
+        t.Error(err)
+    }
+    if !reflect.DeepEqual(receive, sendData) {
+        t.Fatalf("%s is not equal to v:%s", string(receive), string(sendData))
+    }
+}
